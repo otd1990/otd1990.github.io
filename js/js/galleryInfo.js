5821097bcd5afebd206c6c52b4d0e5c2a6a680e2
otd1990 <47889937+otd1990@users.noreply.github.com>
2020-06-20 08:29:18 +0100

    Add files via upload

---
+++ js/js/galleryInfo.js
@@ -0,0 +1,278 @@
+const galleries = [
+  {
+    id: 1,
+    galleryName: 'Landscapes',
+    images: [
+      {
+      name: 'landscapes_1',
+      description: 'Balance, Oil on canvas, 30(h) x 24(d) x 3(w) cm, 2008. SOLD'
+    },
+    {
+      name: 'landscapes_2',
+      description: 'Wendover Woods, Charcoal on paper, 92(h) x 119(d) x 0.1(w) cm, 2007. SOLD',
+    },
+    {
+      name: 'landscapes_3',
+      description: 'Brighton, Oil on canvas, 110(h) x 120(d) x 2.5(w) cm, 2010 (Gift)'
+    },
+    {
+      name: 'landscapes_4',
+      description:'Cannons, Oil on canvas, 30(h) x 24(d) x 3(w) cm, 2008. SOLD'
+    },
+    {
+      name:'landscapes_5',
+      description:'Wendover Woods, Charcoal on paper, 92(h) x 119(d) x 0.1(w) cm, 2007. SOLD'
+    },
+    {
+      name: 'landscapes_6',
+      description:'Wendover Woods, Oil on canvas, 90(h) x 120(d) x 2.5(w) cm, 2007'
+    },
+    {
+      name: 'landscapes_7',
+      description:'Wendover Woods, Charcoal on paper, 109(h) x 128(d) x 0.1(w) cm, 2007. SOLD'
+    },
+    {
+      name: 'landscapes_8',
+      description:'Far Away Places, Oil on canvas, 40(h) x 50(d) x 2(w) cm, 2008. SOLD'
+    },
+    {
+      name: 'landscapes_9',
+      description:'Hut, Oil on canvas, 40(h) x 50(d) x 2(w) cm, 2008. £250'
+    },
+    {
+      name: 'landscapes_12',
+      description:'Wendover Woods, Oil on canvas, 130(h) x 90(d) x 2.5(w) cm, 2017. SOLD'
+    },
+    {
+      name: 'landscapes_13',
+      description:'Seascape, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2017 (Gift)'
+    },
+    {
+      name: 'landscapes_14',
+      description:'Untitled, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2008 (Gift)'
+    },
+    {
+      name: 'landscapes_15',
+      description:'Mountains, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2012 (Gift)'
+    },
+    {
+      name: 'landscapes_16',
+      description:'House, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2012 (Gift)'
+    },
+    {
+      name: 'landscapes_18',
+      description:'Untitled, Oil on board, 20(h) x 24(d) x 3(w) cm (Framed), 2007. £170'
+    },
+    {
+      name: 'landscapes_19',
+      description:'Wendover Woods, Charcoal on paper, 101(h) x 115(d) x 0.1(w) cm, 2007. SOLD'
+    },
+    {
+      name: 'landscapes_20',
+      description:'Wendover Woods, Oil on canvas, 120(h) x 120(d) x 2.5(w) cm, 2007. SOLD'
+    },
+    {
+      name: 'landscapes_21',
+      description:'Path, Oil on board, 15(h) x 20(d) x 3(w) cm (Framed), 2007. £150'
+    },
+    {
+      name: 'landscapes_22',
+      description:'Autumn (Wendover Woods), Oil on canvas, 40(h) x 50(d) x 2(w) cm, 2008. SOLD'
+    },
+    {
+      name: 'landscapes_23',
+      description:'Walking Away, Oil on canvas, 40(h) x 50(d) x 2(w) cm, 2008. SOLD'
+    },
+    {
+      name: 'landscapes_24',
+      description:'Street Scene, Oil on canvas, 30(h) x 24(d) x 3(w) cm, 2008. SOLD'
+    },
+    {
+      name: 'landscapes_25',
+      description:'Valley, Oil on canvas, 40(h) x 50(d) x 2(w) cm, 2012. SOLD'
+    },
+    {
+      name: 'commissions_5',
+      description: 'Street scene, Rome,  Oil on canvas, 40(h) x 50(d) x 2(w) cm, 2015 (Gift)'
+    }
+  ]
+},
+{
+  id: 2,
+  galleryName: 'Norfolk Landscapes',
+  images: [
+    {
+      name: 'nflandscapes_0',
+      description: 'Cley, Oil on canvas, 60(h) x 79.5(d) x 3.5(w) cm, 2020. £450'
+    },
+    {
+      name: 'nflandscapes_1',
+      description: 'Dusk on the Broads, Oil on canvas, 60.3(h) x 104.7(d) x 2(w) cm, 2020. £350'
+    },
+    {
+      name: 'nflandscapes_2',
+      description: 'Castle Acre, Oil on board, 15(h) x 20(d) x 0.5(w) cm, 2018 (Gift)'
+    },
+    {
+      name: 'nflandscapes_3',
+      description: 'Sunset in Blakeney, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2020. £250'
+    },
+  ]
+},
+  {
+    id: 3,
+    galleryName: 'Commissions',
+    images: [
+    {
+      name: 'commissions_1',
+      description: 'Winchester, Oil on canvas, 60(h) x 80(d) x 3(w) cm, 2010'
+    },
+    {
+      name: 'commissions_2',
+      description: 'Weedon, Oil on canvas, 170(h) x 120(d) x 2.5(w) cm, 2012'
+    },
+    {
+      name: 'commissions_3',
+      description: 'Malswick, Oil on canvas, 24(h) x 30(d) x 1.5(w) cm, 2010'
+    },
+    {
+      name: 'commissions_4',
+      description: 'Gran Canaria, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2016'
+    },
+    {
+      name:'commissions_6',
+      description: 'Sea scape with Boats, Oil on board, 60(h) x 70(d) x 0.3(w) cm, 2018'
+    },
+    {
+      name: 'commissions_7',
+      description: 'Playa Del Duque, Oil on canvas, 60(h) x 70(d) x 1.5(w) cm, 2017'
+    },
+    {
+      name: 'commissions_9',
+      description: 'France, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2017'
+    },
+    {
+      name: 'commissions_10',
+      description: 'Aerial Beach, Oil on canvas, 80(h) x 60(d) x 3(w) cm, 2019'
+    },
+    {
+      name: 'landscapes_17',
+      description: 'Brighton, Oil on canvas, 45(h) x 60(d) x 2(w) cm, 2010'
+    },
+    {
+      name: 'landscapes_10',
+      description: 'Winchester, Oil on canvas, 60(h) x 80(d) x 3(w) cm, 2010'
+    },
+    {
+      name: 'landscapes_11',
+      description: 'Wendover Woods, Oil on canvas, 120(h) x 170(d) x 2.5(w) cm, 2013'
+    },
+    {
+      name: 'animals_3',
+      description: 'At the Beach, Oil on canvas, 40(h) x 50(d) x 1.5(w) cm, 2018'
+    },
+  ]
+  },
+  {
+    id: 4,
+    galleryName: 'Pet Portraits',
+    images: [
+      {
+        name: 'pets_1',
+        description: 'Bruce, Watercolour on paper, 29(h) x 21(d) x 0.1(w) cm, 2020 (Gift)'
+      },
+      {
+        name: 'pets_2',
+        description: 'Maisey, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2018 (Commission)'
+      },
+      {
+        name: 'pets_3',
+        description: 'Rosie, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2019 (Gift)'
+      },
+      {
+        name: 'pets_4',
+        description: 'Little Lady, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2017 (Commission)'
+      },
+      {
+        name: 'pets_5',
+        description: 'Rupert, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2017 (Commission)'
+      },
+    ]
+  },
+  {
+    id: 5,
+    galleryName: 'Portraits',
+    images: [
+      {
+        name: 'portraits_1',
+        description: 'Swing, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012 (Commission)'
+      },
+      {
+        name: 'portraits_2',
+        description: 'Pencil on paper, 32(h) x 25(d) x 0.1(w) cm, 2019 (Commission)'
+      },
+      {
+        name: 'portraits_3',
+        description: 'Amelie, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2018 (Gift)'
+      },
+      {
+        name: 'portraits_4',
+        description: 'Lily, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2018 (Gift)'
+      },
+  ]
+  },
+  {
+    id: 6,
+    galleryName: 'Drawings',
+    images: [
+      {
+        name: 'commissions_8',
+        description: 'Mallard Steam Train, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012 (Gift)'
+      },
+      {
+        name: 'animals_1',
+        description: 'Stag, Pencil on paper, 50(h) x 40(d) x 0.1(w) cm, 2016 (Gift)'
+      },
+      {
+        name: 'animals_2',
+        description: 'Two for Joy, Watercolour and pencil on paper, 50(h) x 50(d) x 0.1(w) cm, 2017 (Gift)'
+      },
+      {
+        name: 'preraphaelites_1',
+        description: 'Copy of ‘Boreas’  by John William Waterhouse, Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2016 (Gift)'
+      },
+      {
+        name: 'preraphaelites_2',
+        description: 'Copy of ‘The Day Dream’  by Dante Gabriel Rossetti, Watercolour on paper, 29(h) x 21(d) x 0.1(w) cm, 2003 (Gift)'
+      },
+      {
+        name: 'preraphaelites_3',
+        description: 'Copy of ‘Beata Beatrix’ by Dante Gabriel Rossetti, Watercolour on paper, 29(h) x 21(d) x 0.1(w) cm, 2003'
+      },
+      {
+        name: 'women_1',
+        description: 'Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012'
+      },
+      {
+        name: 'women_2',
+        description: 'Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012'
+      },
+      {
+        name: 'women_3',
+        description: 'Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012'
+      },
+      {
+        name: 'women_4',
+        description: 'Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012'
+      },
+      {
+        name: 'women_5',
+        description: 'Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012'
+      },
+      {
+        name: 'women_6',
+        description: 'Pencil on paper, 29(h) x 21(d) x 0.1(w) cm, 2012'
+      },
+    ]
+  }
+];
